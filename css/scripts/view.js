449b68ac903b86eb79b577a3feab258db7c50379
AkhileshRamchurn <yudishramchurn2599@gmail.com>
2021-04-23 22:04:53 +0400

    Rating system -first commit

---
+++ css/scripts/view.js
@@ -0,0 +1,117 @@
+
+
+$(document).ready(function(){
+    var action="";
+    var video_id="";
+        
+    //if user clicks on the like button
+    $('.like-btn').on('click',function(){
+        video_id = $(this).data('id'); //receives data from data-id (line 195)
+        $clicked_btn = $(this);
+          
+        //if user likes the video
+        if($clicked_btn.hasClass('fa-thumbs-o-up')){
+            action ='like';
+        }
+        //else if user unlike video
+        else if($clicked_btn.hasClass('fa-thumbs-up')){
+            action='unlike';
+        }
+
+
+        //using ajax to make changes without reloading
+    $.ajax({
+        url: 'view.php',
+        type: 'post',
+       // async: true,
+        data: {
+            'action': action,
+            'video_id': video_id
+        },
+
+        //if the response is successful from server
+        success: function(data){
+         alert(data);
+           res = JSON.parse(data); 
+
+           if(action == 'like'){
+               $clicked_btn.removeClass('fa-thumbs-o-up');
+               $clicked_btn.addClass('fa-thumbs-up');
+           }else if(action == "unlike"){
+               $clicked_btn.removeClass('fa-thumbs-up');
+               $clicked_btn.addClass('fa-thumbs-o-up');
+           }
+
+         //display the number of likes and dislikes
+         
+           $clicked_btn.siblings('span.likes').text(res.likes);
+           $clicked_btn.siblings('span.dislikes').text(res.dislikes);
+
+           //change button styling of the other button if the user is reacting the second time to video
+           $clicked_btn.siblings('i.fa-thumbs-down').removeClass('fa-thumbs-down').addClass('fa-thumbs-o-down')
+        }
+       });
+
+       
+
+
+
+    });
+
+    //if user clicks on the dislike button
+    $('.dislike-btn').on('click',function(){
+        video_id = $(this).data('id'); //receives data from data-id (line 195)
+        $clicked_btn = $(this);
+
+        //if user likes the video
+        if($clicked_btn.hasClass('fa-thumbs-o-down')){
+            action ='dislike';
+        }
+        //else if user unlike video
+        else if($clicked_btn.hasClass('fa-thumbs-down')){
+            action='undislike';
+        }
+
+        //using ajax to make changes without reloading
+     $.ajax({
+        url: 'view.php',
+        type: 'post',
+       // async: true,
+        data: {
+            'action': action,
+            'video_id': video_id
+        },
+
+        //if the response is successful from server
+        success: function(data){
+            alert(data)
+           res = JSON.parse(data); 
+
+           if(action == 'dislike'){
+               $clicked_btn.removeClass('fa-thumbs-o-down');
+               $clicked_btn.addClass('fa-thumbs-down');
+           }else if(action == "undislike"){
+               $clicked_btn.removeClass('fa-thumbs-down');
+               $clicked_btn.addClass('fa-thumbs-o-down');
+           }
+
+           //display the number of likes and dislikes
+          $clicked_btn.siblings('span.likes').text(res.likes);
+           $clicked_btn.siblings('span.dislikes').text(res.dislikes);
+
+           //change button styling of the other button if the user is reacting the second time to video
+           $clicked_btn.siblings('i.fa-thumbs-up').removeClass('fa-thumbs-up').addClass('fa-thumbs-o-up')
+        }
+    });
+
+
+    });
+
+
+    
+
+
+    
+
+    
+});
--- view.php
+++ view.php
@@ -1,10 +1,201 @@
+<?php
+	session_start();
+	$user_id =$_SESSION["User_ID"];
+	
+	if(isset($_SESSION["User_ID"])){
+		 
+		
+		//checks if user already like the post
+		function userLiked($video_id, $user_id){				
+			
+			//Retrieve video from table rating
+			include 'dbconnect.php';
+			$sql = "SELECT COUNT(*) FROM rating WHERE USER_ID = $user_id
+			AND VIDEO_ID = $video_id AND Rate_Action ='like'";
+
+			$stmt = $conn -> prepare($sql); 
+			$stmt ->execute();
+			$already_liked =$stmt -> fetchColumn();
+
+			if($already_liked > 0){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+
+		//checks if user already dislike the post
+		function userDisLiked($video_id, $user_id){				
+			
+			//Retrieve video from table rating
+			include 'dbconnect.php';
+			$sql = "SELECT COUNT(*) FROM rating WHERE USER_ID = $user_id
+			AND VIDEO_ID = $video_id AND Rate_Action ='dislike'";
+
+			$stmt = $conn -> prepare($sql); 
+			$stmt ->execute();
+			$already_liked =$stmt -> fetchColumn();
+
+			if($already_liked > 0){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+
+		//Get total number of likes for a particular post
+		function getLikes($id){
+
+			include 'dbconnect.php';
+			$sql = "SELECT COUNT(*) FROM rating WHERE VIDEO_ID = $id AND Rate_Action='like'";
+			$stmt = $conn -> prepare($sql); 
+			$stmt ->execute();
+
+			$numOfLikes= $stmt -> fetchColumn();
+
+			return $numOfLikes;
+
+		}
+
+		//Get total number of likes for a particular post
+		function getDisLikes($id){
+
+			include 'dbconnect.php';
+			$sql = "SELECT COUNT(*) FROM rating WHERE VIDEO_ID = $id AND Rate_Action='dislike'";
+			$stmt = $conn -> prepare($sql); 
+			$stmt ->execute();
+
+			$numOfLikes= $stmt -> fetchColumn();
+			
+			return $numOfLikes;
+
+		}
+
+		//Get total number of likes and dislikes for a particular video
+
+		// function getRating($id){
+		// 	include 'dbconnect.php';
+		// 	console.log("Working here");
+
+		// 	$rating = array();
+
+		// 	$likes_query = "SELECT COUNT(*) FROM rating WHERE Video_ID = $id AND Rate_Action='like'";
+		// 	$dislikes_query = "SELECT COUNT(*) FROM rating WHERE Video_ID = $id AND Rate_Action='dislike'";
+
+		// 	$likes_rs = $conn -> prepare($likes_query); 
+		// 	$dislike_rs = $conn -> prepare($dislikes_query); 
+
+		// 	$likes_rs ->execute();
+		// 	$dislikes_rs ->execute();
+
+		// 	$likes = $likes_rs->fetch();
+		// 	$dislikes = $dislikes_rs->fetch();
+
+		// 	$rating = [
+		// 		'likes' => $likes,
+		// 		'dislikes' => $dislikes
+		// 	];
+
+			
+
+		// 	return json_encode($rating);
+			
+			
+		// }
+
+		//request from ajax
+		//if user clicks like or dislike button
+		if(isset($_POST['action'])){
+			//alert('receiving till here');
+			
+			include 'dbconnect.php';
+			$video_id = $_POST['video_id'];
+			$action = $_POST['action'];
+
+			//echo $action;
+
+			switch($action){
+				case 'like':
+					$sql = "INSERT INTO rating (User_ID, Video_ID, Rate_Action)
+							VALUES($user_id, $video_id, 'like');
+							 ON DUPLICATE KEY UPDATE Rating_Action ='like'";
+					break;
+				
+				case 'dislike':
+					$sql = "INSERT INTO rating (User_ID, Video_ID, Rate_Action)
+						VALUES($user_id, $video_id, 'dislike')
+						--  ON DUPLICATE KEY UPDATE Rating_Action ='dislike'";
+					break;
+				case 'unlike':
+					$sql ="DELETE FROM rating WHERE User_ID=$user_id AND Video_ID = $video_id";					
+					break;
+				case 'undislike':
+					$sql ="DELETE FROM rating WHERE User_ID=$user_id AND Video_ID = $video_id";					
+					break;	
+				default:
+					break;	
+
+			}
+
+			//execute query to effect changes in the database
+			 $stmt = $conn -> prepare($sql); 
+			 $stmt ->execute();
+
+			
+			/*Calculate likes and dislke....Then return back in JSON format */
+			 $rating = array();
+
+			 $likes_query = "SELECT COUNT(*) FROM rating WHERE Video_ID = $user_id AND Rate_Action='like'";
+			 $dislikes_query = "SELECT COUNT(*) FROM rating WHERE Video_ID = $user_id  AND Rate_Action='dislike'";
+
+			 $likes_rs = $conn -> prepare($likes_query); 
+			 $dislikes_rs = $conn -> prepare($dislikes_query); 
+
+			 $likes_rs ->execute();
+			 $dislikes_rs ->execute();
+
+			$likes = $likes_rs->fetchColumn();
+			$dislikes = $dislikes_rs->fetchColumn();
+
+			$rating = [
+		 	'likes' => $likes,
+			'dislikes' => $dislikes
+			];
+
+			
+
+			echo json_encode($rating);
+
+			//echo "fuck you";
+
+			exit(0);
+
+		}
+
+		
+
+		
+	}
+
+	
+	
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>view</title>
 
 	<link rel="stylesheet" href="css/styling.css">
+	<link rel="stylesheet" href="css/view.css">
 	<script src="https://kit.fontawesome.com/260e4ed8bc.js" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<!-- <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script> -->
+	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxy/1.6.1/scripts/jquery.ajaxy.min.js" integrity="sha512-bztGAvCE/3+a1Oh0gUro7BHukf6v7zpzrAb3ReWAVrt+bVNNphcl2tDTKCBr5zk7iEDmQ2Bv401fX3jeVXGIcA==" crossorigin="anonymous"></script> -->
 </head>
 <body>
 
@@ -75,7 +266,8 @@
 
 				 $video_id =$_GET['video_id'] ;
 				include 'dbconnect.php';
-
+				
+				// Retrieve information from table video 
 				$query = "SELECT Title FROM video
 							where video_ID =? ";
 				$stmt = $conn -> prepare($query); 
@@ -96,6 +288,34 @@
 		<div class="video-content">
 			<h3><?php echo $row['Title'] ?></h3>	
 		</div>
+
+		<div class= "video-rating">
+
+			<!-- if user likes post, style button differently -->
+			<i <?php if(userLiked($video_id, $user_id)): ?>
+				 class="fa fa-thumbs-up like-btn"
+
+				<?php else: ?>
+					class="fa fa-thumbs-o-up like-btn"
+				<?php endif ?>	
+				data-id="<?php echo $video_id ?>"></i>
+
+			<span class="likes"><?php echo getLikes($video_id); ?> </span>
+			
+			&nbsp;&nbsp;&nbsp;&nbsp;			
+			<!-- if user dislikes post, style button differently -->
+			<i <?php if(userDisliked($video_id, $user_id)): ?>
+				 class="fa fa-thumbs-down dislike-btn"
+
+				<?php else: ?>
+					class="fa fa-thumbs-o-down dislike-btn"
+				<?php endif ?>	
+				data-id="<?php echo $video_id ?>"></i>
+
+			<span class="likes"><?php echo getDisLikes($video_id); ?> </span>
+		</div>
+
+		
 			
 		
 	</div>
@@ -103,7 +323,7 @@
 
 
 
-
+	<script src="css/scripts/view.js"></script>
 	<!-- script for profile dropdown menu -->
 	
 	<script type="text/javascript">
@@ -115,6 +335,7 @@
 	</script>
 
 
-
 </body>
 </html>
+
+

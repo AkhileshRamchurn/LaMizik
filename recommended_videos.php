6921fb82d8f5a6a995e1d305fbf3ddef76cf5ac3
AkhileshRamchurn <yudishramchurn2599@gmail.com>
2021-06-25 19:50:30 +0400

    final recommend videos

--- recommended_videos.php
+++ recommended_videos.php
@@ -40,9 +40,12 @@
 		$sql1 = "(SELECT Video_ID FROM views WHERE User_ID IN $condition_simUser GROUP BY Video_ID)";
         $sql2 = "(SELECT Video_ID FROM views WHERE User_ID = $user_id GROUP BY Video_ID)";
 
-        $sql = "SELECT a.Video_ID FROM $sql1 a, $sql2 b WHERE a.Video_ID != b.Video_ID";
+        $sql3 = "(SELECT a.Video_ID FROM $sql1 a, $sql2 b WHERE a.Video_ID != b.Video_ID GROUP BY a.Video_ID)";
+        
+        $sqlfinal ="SELECT  video.Video_ID, Title, Description, Video_Type, Status, UNIX_TIMESTAMP(Upload_Timestamp) AS Upload_Timestamp, user.Username FROM $sql3 a, video, user WHERE video.Video_ID = a.Video_ID AND video.User_ID = user.User_ID";
 
-		$result2 = $conn->query($sql);
+
+		$result2 = $conn->query($sqlfinal);
         $uncommon_video_array = $result2->fetchAll(PDO::FETCH_ASSOC);
       
         // $jsonData = json_encode($uncommon_video_array, JSON_PRETTY_PRINT);
@@ -55,12 +58,18 @@
         //     // for($j=0; $)
 
         // }
-        $unwatchVideo_ratings = array();
-        //calculating the predicted score of each video returned    
+       
+        //calculating the predicted score of each video returned
+    
         for($i=0;$i<count($uncommon_video_array); $i++){
             $unwatched_videoid=$uncommon_video_array[$i]['Video_ID'];
+            $video_weightmean=0;
+            $total_video_weightmean=0;
+            $total_similairty=0;
             
             for($j=0;$j<count($similarity_obj[0]['Other_Users']); $j++){
+               
+
                 $sim_userid =$similarity_obj[0]['Other_Users'][$j]['User_ID'];
                 // echo $sim_userid.' ';
                 $query1 = "SELECT * FROM views WHERE USER_ID=$sim_userid AND Video_ID=$unwatched_videoid";
@@ -71,6 +80,7 @@
                 $video_score;
                 if(count($unwatched_videoViewed)){//user has not viewed the video
                     $video_score=1;
+
                 }
                 else{//user has viewed the video
                     $query1= "SELECT Rate_Action FROM rating WHERE Video_ID =$unwatched_videoid  AND User_ID = ".$sim_userid;
@@ -92,33 +102,38 @@
 
                 }
                 // $unwatchVideo_ratings[$i][$j] = 
-
-
+                $total_video_weightmean += $similarity_obj[0]['Other_Users'][$j]['Similarity'] * $video_score;
+                $total_similairty+=$similarity_obj[0]['Other_Users'][$j]['Similarity'];
             }
-                
+            $video_weightmean = $total_video_weightmean/$total_similairty;
+            $uncommon_video_array[$i]['video_weightmean'] = $video_weightmean;
+
+            $views_query ="SELECT count(*) AS numOfViews FROM views WHERE Video_ID=$unwatched_videoid";
+            $result5 = $conn -> query($views_query);
+            $video_views= $result5->fetchAll(PDO::FETCH_ASSOC);
+
+            $uncommon_video_array[$i]['views'] = $video_views[0]['numOfViews'];
+          
         }
-
-
-		//  $user_array[$i]['Videos'] = $result3->fetchAll(PDO::FETCH_ASSOC);
-
-		
+        
+	
 	}
-/*
-    // Sorting the videos based on score in descending order
-    for($i=0; $i<count($user_array); $i++)
-	{
-        usort($user_array[$i]['Videos'], function ($item1, $item2) {
-            return $item2['Score'] <=> $item1['Score'];
+
+
+  
+        usort($uncommon_video_array, function ($item1, $item2) {
+            return $item2['video_weightmean'] <=> $item1['video_weightmean'];
         });
 
         // Getting the top n videos only
-        $user_array[$i]['Videos'] = array_slice($user_array[$i]['Videos'], 0, $video_limit);
-    }
+         $uncommon_video_array = array_slice($uncommon_video_array, 0, $video_limit);
 
-    // $jsonData = json_encode($user_array, JSON_NUMERIC_CHECK);
-    $jsonData = json_encode($user_array, JSON_PRETTY_PRINT);
+
+    $jsonData = json_encode($uncommon_video_array, JSON_NUMERIC_CHECK);
+    $jsonData = json_encode($uncommon_video_array, JSON_PRETTY_PRINT);
+
 
     header('Content-Type: application/json'); 
     echo $jsonData; 
-*/
+
 ?>
